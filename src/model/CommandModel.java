e4484ad861428a096e4b13a67fe496a1359686ad
Evan Kenyon <ek168@duke.edu>
2021-09-27 16:40:49 -0400

    Extracted some methods in CommandModel

--- src/model/CommandModel.java
+++ src/model/CommandModel.java
@@ -1,30 +1,72 @@
 package model;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
-import java.util.regex.PatternSyntaxException;
 
 public class CommandModel {
+  private Scanner scanner;
 
   public CommandModel() {
 
   }
 
-  public void parseInput(String input) throws PatternSyntaxException {
-    Scanner scanner = new Scanner(input);
+  public void parseInput(String input) throws InputMismatchException {
+    scanner = new Scanner(input);
     switch (scanner.next().toLowerCase()) {
-      case "fd" -> System.out.println("Go forward");
-      case "bk" -> System.out.println("Go backward");
-      case "lt" -> System.out.println("Turn left");
-      case "rt" -> System.out.println("Turn right");
+      case "fd" -> handleMovementCommand("forward");
+      case "bk" -> handleMovementCommand("backward");
+      case "lt" -> handleAngleCommand("left");
+      case "rt" -> handleAngleCommand("right");
       case "pd" -> System.out.println("Pen down");
       case "pu" -> System.out.println("Pen up");
       case "st" -> System.out.println("Show turtle");
       case "ht" -> System.out.println("Hide turtle");
       case "home" -> System.out.println("Go home");
       case "stamp" -> System.out.println("Stamp turtle");
-      case "tell" -> System.out.println("Tell turtles");
-      default -> throw new PatternSyntaxException("test", "test", 2);
+      case "tell" -> handleTellCommand();
+      default -> throw new InputMismatchException();
     }
+  }
+
+  private void handleAngleCommand(String direction) throws InputMismatchException{
+    System.out.print("Turn " + direction);
+    System.out.print(parseFirstNumArg());
+    System.out.println(" degrees");
+  }
+
+  private void handleMovementCommand(String direction) throws InputMismatchException{
+    System.out.print("Go " + direction);
+    System.out.print(parseFirstNumArg());
+    System.out.println(" pixels");
+  }
+
+  private void handleTellCommand() {
+    System.out.print("Tell turtles ");
+    System.out.print(parseFirstNumArg());
+    System.out.print(" ");
+    while(scanner.hasNext()) {
+      System.out.print(parseNumInput());
+      System.out.print(" ");
+    }
+    System.out.println();
+  }
+
+  private int parseFirstNumArg() {
+    if (!scanner.hasNext()) {
+      throw new InputMismatchException();
+    }
+    return parseNumInput();
+  }
+
+  private int parseNumInput() {
+    int numInput = - 1;
+    try {
+      numInput = Integer.parseInt(scanner.next());
+    } catch (NumberFormatException e) {
+      // TODO: fix this up
+      e.printStackTrace();
+    }
+    return numInput;
   }
 
 }

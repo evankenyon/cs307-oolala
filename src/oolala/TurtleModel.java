fe2eca8fc31dbe887bf1fe0a06e50c335d20b6ae
Luis Pereda Amaya <lip4@duke.edu>
2021-09-27 21:25:58 -0400

    Small updates to methods

--- src/oolala/TurtleModel.java
+++ src/oolala/TurtleModel.java
@@ -2,9 +2,12 @@ package oolala;
 
 public class TurtleModel {
 
-  private double[] myPosition;
+  private int[] myPosition;
   // A number from 0 to 2pi representing the angle of travel of the turtle
   private double myTrajectory;
+  // If true, pen is down and drawing; if false, pen is up and not drawing
+  private boolean penSetting;
+  private int[] myHome;
 
   /**
    * Create a TurtleModel with the initial where we want the turtle to be initialized on the screen.
@@ -12,13 +15,18 @@ public class TurtleModel {
    *
    * @param initialPosition
    */
-  public TurtleModel(double[] initialPosition) {
+  public TurtleModel(int[] initialPosition) {
     myPosition = initialPosition;
+    myTrajectory = 0;
+    penSetting = true;
+    myHome = new int[2];
   }
 
   public TurtleModel() {
-    double[] turtlePosition = new double[2];
-    myPosition = turtlePosition;
+    myPosition = new int[2];
+    myTrajectory = 0;
+    penSetting = true;
+    myHome = new int[2];
   }
 
   /**
@@ -26,22 +34,62 @@ public class TurtleModel {
    *
    * @return
    */
-  public double[] getPosition() {
+  public int[] getPosition() {
     return myPosition;
   }
 
   /**
-   * Moves the turtle based on the distance and angle provided by the user
+   * Getter for the angle at which the turtle is moving
+   *
+   * @return
+   */
+  public double getTrajectory() {
+    return myTrajectory;
+  }
+
+  /**
+   * Moves the turtle based on the distance provided by the user
    *
    * @param distance
-   * @param angle
    */
-  public void move(double distance, double angle) {
-    double angleInRadians = angle * Math.PI / 180;
-    double dx = distance * Math.cos(angleInRadians);
-    double dy = distance * Math.sin(angleInRadians);
+  public void move(int distance) {
+    int dx = distance * (int) Math.cos(myTrajectory);
+    int dy = distance * (int) Math.sin(myTrajectory);
     myPosition[0] = myPosition[0] + dx;
     myPosition[1] = myPosition[1] + dy;
   }
 
+  /**
+   * Rotate the turtle based on the angle provided by the user
+   *
+   * @param angle
+   */
+  public void rotate(int angle) {
+    myTrajectory = angle * Math.PI / 180;
+  }
+
+  /**
+   * Set whether the pen is drawing or not drawing
+   *
+   * @param penUpOrDown
+   */
+  public void setPen(boolean penUpOrDown) {
+    penSetting = penUpOrDown;
+  }
+
+  /**
+   * Set home location for turtle
+   *
+   * @param homeLocation
+   */
+  public void setHome(int[] homeLocation) {
+    myHome = homeLocation;
+  }
+
+  /**
+   * Turtle is moved to home
+   */
+  public void goHome() {
+    myPosition = myHome;
+  }
 }

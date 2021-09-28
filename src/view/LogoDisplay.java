604f7aa3fb3317edd0a20d5cffd70935bacffd4c
Haseeb Chaudhry <hac21@duke.edu>
2021-09-28 04:44:08 -0400

    Did overall LogoDisplay to account for a single turtle object with a view and model

--- src/view/LogoDisplay.java
+++ src/view/LogoDisplay.java
@@ -1,5 +1,35 @@
 package view;
 
+import model.TurtleModel;
+
+import java.net.URL;
+
 public class LogoDisplay {
+    private int ID;
+    private TurtleDisplay turtleDisplay;
+    private TurtleModel turtleModel;
+
+    public LogoDisplay(int id){
+        ID = id;
+        turtleModel = new TurtleModel();
+        turtleDisplay = new TurtleDisplay(turtleModel);
+    }
+
+    public void setTurtleImage(URL url){
+        turtleDisplay.setImage(url);
+    }
+
+    public void move(int dist){
+        turtleDisplay.moveTurtle(dist);
+    }
+
+    public void rotate(int rot){
+        turtleDisplay.rotateTurtle(rot);
+    }
+
+    public void penState(boolean bol){
+        turtleDisplay.turtleDrawing(bol);
+    }
+
 
 }

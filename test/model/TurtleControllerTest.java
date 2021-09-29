5fdbc35a4b6cc7749154d4b91fc161a1b0c06a8f
Luis Pereda Amaya <lip4@duke.edu>
2021-09-29 17:32:49 -0400

    Forgot to add test file on last commit

--- test/model/TurtleControllerTest.java
+++ test/model/TurtleControllerTest.java
@@ -1,4 +1,23 @@
+package model;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class TurtleControllerTest {
-  
+  private TurtleController turtleController;
+  @BeforeEach
+  public void setUp () {
+    turtleController = new TurtleController();
+  }
+  @Test
+  public void testAddTurtles() {
+    turtleController.addTurtle(1);
+    turtleController.addTurtle(2);
+    turtleController.addTurtle(4);
+    int expected = 3;
+
+    assertEquals(expected, turtleController.getActiveTurtles().size());
+  }
 }

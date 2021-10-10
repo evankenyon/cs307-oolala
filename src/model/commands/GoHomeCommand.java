00e7201b1ce691ec147b3f8d5045a0017627bd19
Luis Pereda Amaya <lip4@duke.edu>
2021-10-10 19:53:47 -0400

    JavaDoc for all of the commands

--- src/model/commands/GoHomeCommand.java
+++ src/model/commands/GoHomeCommand.java
@@ -4,14 +4,35 @@ import java.util.List;
 import model.TurtleController;
 import model.TurtleModel;
 
+/**
+ * Purpose: This class implements a command for the turtle to be moved to home.
+ * Assumptions: Args passed to the constructor should be empty.
+ * Dependencies: List, TurtleController, TurtleModel
+ *
+ * Example: If the user types in 'home', create a GoHomeCommand and use the runCommand method to
+ * make the turtle go home.
+ *
+ * @author Luis Pereda and Evan Kenyon
+ */
 public class GoHomeCommand implements Command {
 
+  /**
+   * Purpose: Create a GoHomeCommand.
+   *
+   * @param args should be empty
+   * @throws IllegalArgumentException
+   */
   public GoHomeCommand(List<Integer> args) throws IllegalArgumentException {
     if (!args.isEmpty()) {
       throw new IllegalArgumentException();
     }
   }
 
+  /**
+   * Purpose: Move the turtles to their home locations
+   *
+   * @param turtleController The turtle controller parameter will have a list of active turtles
+   */
   @Override
   public void runCommand(TurtleController turtleController) {
     for (TurtleModel turtleModel : turtleController.getActiveTurtles()) {
--- src/model/commands/MoveCommand.java
+++ src/model/commands/MoveCommand.java
@@ -4,10 +4,29 @@ import java.util.List;
 import model.TurtleController;
 import model.TurtleModel;
 
+/**
+ * Purpose: This class represents an abstraction of implementing a command to move the turtle forward or backward
+ * Assumptions: This class will only be inherited, never instantiated. Classes that make a command to
+ * move forward or to move backward can inherit from this class.
+ * Dependencies: List, TurtleController, TurtleModel
+ *
+ * Example: Create a class that moves the turtle forward or backward that extends this class, use the runCommand method
+ * to move the turtles.
+ *
+ * @author Luis Pereda and Evan Kenyon
+ */
 public abstract class MoveCommand implements Command {
 
   private final int distance;
 
+  /**
+   * Purpose: Create a move command constructor with distance provided by args to be inherited by
+   * subclasses.
+   * Assumptions: Args should have size one.
+   * @param direction 1 if forward, -1 if backward.
+   * @param args List of integers of length 1.
+   * @throws IllegalArgumentException
+   */
   public MoveCommand(int direction, List<Integer> args) throws IllegalArgumentException {
     if (args.size() != 1) {
       throw new IllegalArgumentException();
@@ -15,6 +34,12 @@ public abstract class MoveCommand implements Command {
     distance = direction * args.get(0);
   }
 
+  /**
+   * Purpose: This method moves all the active turtles in the turtle controller the
+   * distance specified by the user.
+   *
+   * @param turtleController The turtle controller parameter will have a list of active turtles
+   */
   @Override
   public void runCommand(TurtleController turtleController) {
     for (TurtleModel turtleModel : turtleController.getActiveTurtles()) {

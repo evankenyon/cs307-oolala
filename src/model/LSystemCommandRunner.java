48ed86a658db45eef99abb2c47c954e77820d426
Evan Kenyon <ek168@duke.edu>
2021-10-12 01:56:19 -0400

    Documented LSystemProgram

--- src/model/LSystemCommandRunner.java
+++ src/model/LSystemCommandRunner.java
@@ -9,15 +9,18 @@ import model.commands.*;
 import view.TurtleWindowDisplay;
 
 /**
- * Purpose: Represent an LSystemCommandModel, which takes in symbols and parses them into
- * Command objects
- * Dependencies: ArrayList, HashMap, InputMismatchException, List, Map, commands, TurtleWindowDisplay
- * Example: Instantiate an LSystemCommandRunner in LSystemsModel in order to get symbols from
- * LSystemProgram's symbol generation functionality, and return commands based on those symbols
- * Other details: LSystemProgram handles the parsing of set commands, and LSystemsModel updates
- * the map in this class based on LSystemProgram's parsing
+ * Purpose: Represent an LSystemCommandModel, which takes in symbols and parses them into Command
+ * objects Dependencies: ArrayList, HashMap, InputMismatchException, List, Map, commands,
+ * TurtleWindowDisplay Example: Instantiate an LSystemCommandRunner in LSystemsModel in order to get
+ * symbols from LSystemProgram's symbol generation functionality, and return commands based on those
+ * symbols Other details: LSystemProgram handles the parsing of set commands, and LSystemsModel
+ * updates the map in this class based on LSystemProgram's parsing
+ *
+ * @author Haseeb Chaudhry
+ * @author Evan Kenyon
  */
 public class LSystemCommandRunner extends CommandModel {
+
   private int movementLength;
   private int rotationAngle;
   private List<Integer> originalLocation;
@@ -29,9 +32,11 @@ public class LSystemCommandRunner extends CommandModel {
 
   /**
    * Purpose: Construct an LSystemCommandRunner object
-   * Assumptions: distance and angle are reasonable values (i.e. distance is not greater than the screen length)
-   * @param distance initial movement command length
-   * @param angle initial rotate command angle
+   * Assumptions: distance and angle are
+   * reasonable values (i.e. distance is not greater than the screen length)
+   *
+   * @param distance       initial movement command length
+   * @param angle          initial rotate command angle
    * @param totalNumLevels initial number of levels that should be generated
    */
   public LSystemCommandRunner(int distance, int angle, int totalNumLevels) {
@@ -58,12 +63,15 @@ public class LSystemCommandRunner extends CommandModel {
   }
 
   /**
-   * Purpose: Further parses the new symbol set command and adds the symbol to command mapping to the inputCharToCommand map (or replaces it)
-   * Assumptions: newSymbolSet represents a partially parsed set command from LSystemProgram, which originally took in a user inputted set command
+   * Purpose: Further parses the new symbol set command and adds the symbol to command mapping to
+   * the inputCharToCommand map (or replaces it).
+   * Assumptions: newSymbolSet represents a partially
+   * parsed set command from LSystemProgram, which originally took in a user inputted set command.
+   *
    * @param newSymbolSet a partially parsed set command from LSystemProgram
    */
   public void putInInputCharToCommand(Map<String, List<String>> newSymbolSet) {
-    for(String symbol : newSymbolSet.keySet()) {
+    for (String symbol : newSymbolSet.keySet()) {
       String parsedCommand = String.join(" ", newSymbolSet.get(symbol));
       parsedCommand = parsedCommand.replace("\"", "");
       parsedCommand = parsedCommand.toLowerCase();
@@ -75,7 +83,6 @@ public class LSystemCommandRunner extends CommandModel {
   }
 
   /**
-   *
    * @param input
    * @return
    * @throws InputMismatchException
@@ -83,12 +90,12 @@ public class LSystemCommandRunner extends CommandModel {
   @Override
   public List<Command> getCommandsFromInput(String input) throws InputMismatchException {
     List<Command> commands = new ArrayList<>();
-    if(isFirstCommand) {
+    if (isFirstCommand) {
       currLocation = new ArrayList<>(originalLocation);
       commands.addAll(goToStartLocation());
       isFirstCommand = false;
     }
-    for(String inputChar : input.split("(?!^)")) {
+    for (String inputChar : input.split("(?!^)")) {
       commands.addAll(getCommand(inputChar));
     }
     currLocation.set(1, currLocation.get(1) + levelIncrement);
@@ -114,16 +121,18 @@ public class LSystemCommandRunner extends CommandModel {
   /**
    * Purpose: Set the start location of the turtle to inputStartLocation and reset levelIncrement
    * based on the new start location.
-   * Assumptions: totalNumLevels is a reasonable value (i.e. much smaller than inputStartLocations
-   * y value).
+   * Assumptions: totalNumLevels is a reasonable value (i.e. much
+   * smaller than inputStartLocations y value).
+   *
    * @param inputStartLocation the x and y values to set this class's start location to.
-   * @param totalNumLevels the total number of levels to draw.
+   * @param totalNumLevels     the total number of levels to draw.
    */
   public void setStartLocation(int[] inputStartLocation, int totalNumLevels) {
     originalLocation.clear();
     originalLocation.add(inputStartLocation[0]);
     originalLocation.add(inputStartLocation[1]);
-    levelIncrement = (TurtleWindowDisplay.PREF_WINDOW_SIZE - originalLocation.get(1)) / totalNumLevels;
+    levelIncrement =
+        (TurtleWindowDisplay.PREF_WINDOW_SIZE - originalLocation.get(1)) / totalNumLevels;
   }
 
   /**
